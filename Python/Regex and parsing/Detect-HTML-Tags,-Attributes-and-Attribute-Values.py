d2859be4c910d32410bb3b8def2d8ecd8cab4388
Imam Suyuti <imamsuyuti00@student.uns.ac.id>
2022-10-11 19:33:47 +0700

    Detect HTML Tags, Attributes and Attribute Values solution

---
+++ Python/Regex and parsing/Detect-HTML-Tags,-Attributes-and-Attribute-Values.py
@@ -0,0 +1,16 @@
+from html.parser import HTMLParser
+
+class MyHTMLParser(HTMLParser):
+  def handle_starttag(self, tag, attrs):
+    print(tag)
+    for attr in attrs:
+      print("-> {} > {}".format(attr[0], attr[1]))
+      
+html = ""       
+for i in range(int(input())):
+    html += input().rstrip()
+    html += '\n'
+    
+parser = MyHTMLParser()
+parser.feed(html)
+parser.close()
